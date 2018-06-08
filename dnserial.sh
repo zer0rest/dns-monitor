2a82a5b7c4cf32760608f9d0d692fc41f79f3786
zer0rest <github@zer0rest.net>
2018-06-08 18:18:15 +0300

    Added domain SOA serial number monitoring script.

---
+++ dnserial.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#Script to check for changes in the domain's SOA record serial number.
+#Receives the (sub)domain as argument when the script is called.
+
+#Check if a (sub)domain has been passed as the first argument
+#If that's the case, find its serial number.
+DOMAIN=$1
+
+if  [[ $1 ]]
+then
+	SERIAL=$(dig +short soa $DOMAIN | awk '{printf $3}')
+else
+	echo "No (sub)domain has been passed as an argument."
+fi
+
+#Check if old SOA record exists, if it doesn't create it.
+if [ ! -e $DOMAIN.serial.txt ]
+then
+	touch $DOMAIN.serial.txt
+	echo "serial.txt didn't exist, created it."
+	#Since this is the first run, add the serial number to serial.txt
+	echo $SERIAL > $DOMAIN.serial.txt
+fi
+
+OLDSERIAL=$(cat $DOMAIN.serial.txt)
+
+#Check if the old serial number is not the same as the new serial number.
+#If that's the case notify the user.
+
+if [[ $SERIAL != $OLDSERIAL ]]
+then
+	#Since the serial number has changed, add the update serial number to serial.txt for
+	#the next time the script runs.
+	echo $SERIAL > $DOMAIN.serial.txt
+
+	#Define the message to transmit when the serial number has changed.
+	MESSAGE="According to dnserial.sh, the serial number for $DOMAIN has changed from $OLDSERIAL to $SERIAL"
+
+	#Publish a notification to the specified mqtt channel.
+	# $MQTT_SERVER_DOMAIN is an enviroment variable that stores the mqtt server domain/ip
+	# $MQTT_TOPIC is an enviroment variable that stores the mqtt topic updates will be published to.
+	mosquitto_pub -h "$MQTT_SRV" -t "$MQTT_TOPIC" -m "$MESSAGE"
+fi
